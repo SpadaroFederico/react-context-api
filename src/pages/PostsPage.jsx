edd146ce0e4029c2d3e4e44781a8335778c695ef
Federico Spadaro <federicospadaro2@gmail.com>
2025-02-26 17:26:13 +0100

    [DONE] postspages

---
+++ src/pages/PostsPage.jsx
@@ -0,0 +1,12 @@
+import PostsList from "../components/PostsList";
+
+function PostsPage() {
+  return (
+    <div>
+      <h1>Lista dei Post</h1>
+      <PostsList />
+    </div>
+  );
+}
+
+export default PostsPage;
